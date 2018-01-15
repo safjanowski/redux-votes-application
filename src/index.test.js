d8d1728daa709365ab98f1ac3fe4e327eaaca807
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2018-01-15 22:49:12 +0100

    add tests for double dispatch action

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,7 @@ const mocks = require('../mocks')
 const createStore = require('./index')
 const expect = require('chai').expect
 
+const getNoteById = (store, id) => store.getState().find(note => note.id === id)
 
 context(`THUMB_UP`, () => {
   let store
@@ -10,12 +11,30 @@ context(`THUMB_UP`, () => {
     store = createStore(mocks)
   })
 
-  it('updates votes with +1', () => {
+  it('single dispatch `votes` to with value `1`', () => {
+    const id = 'b5e558f0-999b-40b7-b387-4f60fa421815'
+    
     store.dispatch({
       type: 'THUMB_UP',
-      id: 'b5e558f0-999b-40b7-b387-4f60fa421815'
+      id
     })
 
-    expect(store.getState().find(note => note.id === 'b5e558f0-999b-40b7-b387-4f60fa421815').votes).to.equal(1)
+    expect(getNoteById(store, id)).to.have.property('votes').to.equal(1)
+  })
+
+  it('double dispatches updates `votes` with value `2`', () => {
+    const id = 'b5e558f0-999b-40b7-b387-4f60fa421815'
+    
+    store.dispatch({
+      type: 'THUMB_UP',
+      id
+    })
+
+    store.dispatch({
+      type: 'THUMB_UP',
+      id
+    })
+
+    expect(getNoteById(store, id)).to.have.property('votes').to.equal(2)
   })
 })
